ab509b96ffa5138a7438427416570159ede6b8fe
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-03-26 17:16:32 +0100

    flakify

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "nix-cmake-py-test";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        shell =
+          with pkgs;
+          mkShell {
+            packages = [
+              cmake
+              python3
+            ];
+          };
+      in
+      {
+        devShells.default = shell;
+      }
+    );
+}
